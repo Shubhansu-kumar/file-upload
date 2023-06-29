7a6420fa5567f35700688d0b15b75920f51a185f
Shubhansu <shubhansu.kumar@innodeed.com>
2023-06-29 20:27:04 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useDropzone } from "react-dropzone";
 
-function App() {
+const App = () => {
+  const [image, setImage] = useState([]);
+
+  const handleDrop = (acceptedFiles) => {
+    const images = acceptedFiles.map((file) =>
+      Object.assign(file, {
+        preview: URL.createObjectURL(file),
+      })
+    );
+
+    setImage((prevImages) => [...prevImages, ...images]);
+  };
+
+  const removeImage = (index) => {
+    setImage((selImage) => {
+      const updatedImages = [...selImage];
+      updatedImages.splice(index, 1);
+      return updatedImages;
+    });
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    onDrop: handleDrop,
+  });
+
+  const imagePreviews = image.map((image, index) => (
+    <div key={index} style={{margin : '10px'}}>
+      <img src={image.preview} width={"300px"} />
+      <button onClick={() => removeImage(index)}>Remove</button>
+    </div>
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        {...getRootProps()}
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          border: "2px solid black",
+          margin: "13px",
+          justifyContent:'space-around',
+          backgroundColor:'lightblue'
+        }}
+      >
+        <input {...getInputProps()} />
+        <p>Click to Select The Images</p>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "flex-end",
+          justifyContent: "space-evenly",
+        }}
+      >
+        {imagePreviews}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
